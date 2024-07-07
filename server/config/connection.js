4f157b567f3195963001d683679019072b55c069
Jonathan Winter <jonpw2004@outlook.com>
2024-07-07 15:49:50 -0600

    switched from mongoose to sql

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,15 @@
-const mongoose = require('mongoose');
+require('dotenv').config();
 
-//mongoose.connect(process.env.MONGODB_URI || 'tbd');
+const Sequelize = require('sequelize');
 
-module.exports = mongoose.connection;
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
+
+module.exports = sequelize;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,14 @@
 const express = require('express')
-const path = require('path')
+const routes = require('./routes');
 
-const db = require('./config/connection')
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
